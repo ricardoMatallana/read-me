c61e38bf782f8108f5ad3c7502b19b91fae08753
Ricardo Matallana <164824520+ricardoMatallana@users.noreply.github.com>
2024-04-15 20:50:12 -0400

    Add files via upload

---
+++ authentication_Logic.js
@@ -0,0 +1,16 @@
+const pool = require('../config/db');
+
+const findUserByUsername = async (username) => {
+  const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+  return result.rows[0];
+};
+
+const createUser = async (username, password) => {
+  const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, password]);
+  return result.rows[0];
+};
+
+module.exports = {
+  findUserByUsername,
+  createUser
+};
