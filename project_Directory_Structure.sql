24b97ede828d4ba6d98e0b58be27d4f8205d4c55
Ricardo Matallana <164824520+ricardoMatallana@users.noreply.github.com>
2024-04-15 20:40:36 -0400

    Add files via upload
    Project Directory Structure

---
+++ project_Directory_Structure.sql
@@ -0,0 +1,13 @@
+/secure-video-service
+|-- /node_modules
+|-- /src
+|   |-- /config
+|   |   `-- db.js
+|   |-- /controllers
+|   |   `-- authController.js
+|   |-- /models
+|   |   `-- userModel.js
+|   |-- /routes
+|   |   `-- authRoutes.js
+|   `-- server.js
+|-- package.json
